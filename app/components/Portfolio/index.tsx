bcde0a3bc621ad40e9ef969395ede434d6e7287e
PlanetGit <topnotchdev@outlook.com>
2023-10-27 16:07:24 +0900

    Implemented Portfolio content

---
+++ app/components/Portfolio/index.tsx
@@ -0,0 +1,84 @@
+import { Box, Button, Grid, GridItem, Heading, Image, Text } from "@chakra-ui/react";
+
+export default function Portfolio() {
+  return (
+    <>
+      <Box p={4}>
+        <Heading as='h3' size='md' mt={20} color='blue' textAlign={'center'} textTransform={"uppercase"}>
+          Latest work
+        </Heading>
+        <Heading as='h2' size='2xl' textAlign={'center'} mt={10}>
+          My portfolio
+        </Heading>
+        <Grid templateColumns='repeat(3, 1fr)' gap='10' py={20}>
+          <GridItem>
+            <Box boxShadow='lg' p='6' rounded='md' bg='white'>
+              <Image src='/images/about.jpg' borderRadius={10} w='100%' h='auto' objectFit='cover' alt='Hero' />
+              <Text as='p' size='md' mt={2} color='blue'>
+                Data Visualization
+              </Text>
+              <Heading as='h4' size='md' color='black' mt={4}>
+                Tourist booking application
+              </Heading>
+            </Box>
+          </GridItem>
+          <GridItem>
+            <Box boxShadow='lg' p='6' rounded='md' bg='white'>
+              <Image src='/images/about.jpg' borderRadius={10} w='100%' h='auto' objectFit='cover' alt='Hero' />
+              <Text as='p' size='md' mt={2} color='blue'>
+                Mockup Design
+              </Text>
+              <Heading as='h4' size='md' color='black' mt={4}>
+                Smart Mobile Application
+              </Heading>
+            </Box>
+          </GridItem>
+          <GridItem>
+            <Box boxShadow='lg' p='6' rounded='md' bg='white'>
+              <Image src='/images/about.jpg' borderRadius={10} w='100%' h='auto' objectFit='cover' alt='Hero' />
+              <Text as='p' size='md' mt={2} color='blue'>
+                UI/UX Art
+              </Text>
+              <Heading as='h4' size='md' color='black' mt={4}>
+                Daily Health Application
+              </Heading>
+            </Box>
+          </GridItem>
+          <GridItem>
+            <Box boxShadow='lg' p='6' rounded='md' bg='white'>
+              <Image src='/images/about.jpg' borderRadius={10} w='100%' h='auto' objectFit='cover' alt='Hero' />
+              <Text as='p' size='md' mt={2} color='blue'>
+                Mockup Design
+              </Text>
+              <Heading as='h4' size='md' color='black' mt={4}>
+                Mobile Application Build
+              </Heading>
+            </Box>
+          </GridItem>
+          <GridItem>
+            <Box boxShadow='lg' p='6' rounded='md' bg='white'>
+              <Image src='/images/about.jpg' borderRadius={10} w='100%' h='auto' objectFit='cover' alt='Hero' />
+              <Text as='p' size='md' mt={2} color='blue'>
+                Data Visualization
+              </Text>
+              <Heading as='h4' size='md' color='black' mt={4}>
+                Wearable Mobile Fitness App
+              </Heading>
+            </Box>
+          </GridItem>
+          <GridItem>
+            <Box boxShadow='lg' p='6' rounded='md' bg='white'>
+              <Image src='/images/about.jpg' borderRadius={10} w='100%' h='auto' objectFit='cover' alt='Hero' />
+              <Text as='p' size='md' mt={2} color='blue'>
+                DevOps Application
+              </Text>
+              <Heading as='h4' size='md' color='black' mt={4}>
+                Tourist booking application
+              </Heading>
+            </Box>
+          </GridItem>
+        </Grid>
+      </Box>
+    </>
+  )
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Hero from '../components/Hero';
 import About from '../components/About';
+import Portfolio from '../components/Portfolio';
 
 export default function Dashboard() {
   return (
@@ -11,6 +12,7 @@ export default function Dashboard() {
       <Container maxW='6xl' >
         <Hero />
         <About />
+        <Portfolio />
       </Container>
       <Footer />
     </>
